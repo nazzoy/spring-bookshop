85ab95002387b21f4873051cafece86f8036da00
nazzoy <nazoyheh@gmail.com>
2025-05-29 02:29:43 +0500

    приступаю к 4.5

--- src/main/java/my/bookshopapp/controllers/MainPageController.java
+++ src/main/java/my/bookshopapp/controllers/MainPageController.java
@@ -1,15 +1,18 @@
 package my.bookshopapp.controllers;
 
+import my.bookshopapp.data.Book;
 import my.bookshopapp.data.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/bookshop")
 public class MainPageController {
 
     private final  BookService bookService;
@@ -20,11 +23,14 @@ public class MainPageController {
     }
 
 
-    //test commit
-    @GetMapping("/main")
-    public String mainPage(Model model){
-        model.addAttribute("bookData", bookService.getBooksData());
-        model.addAttribute("searchPlaceholder", "new search placeholder");
+    @ModelAttribute("recommendedBooks")
+    public List<Book> recommendedBooks(){
+        return bookService.getBooksData();
+    }
+
+
+    @GetMapping("/")
+    public String mainPage() {
         return "index";
     }
 }
