6002399654176c8df8d588e4146f37545c43e29d
nazzoy <nazoyheh@gmail.com>
2025-05-28 21:06:28 +0500

    Initial commit

---
+++ src/main/java/my/bookshopapp/controllers/MainPageController.java
@@ -0,0 +1,28 @@
+package my.bookshopapp.controllers;
+
+import my.bookshopapp.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/bookshop")
+public class MainPageController {
+
+    private final  BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/main")
+    public String mainPage(Model model){
+        model.addAttribute("bookData", bookService.getBooksData());
+        model.addAttribute("searchPlaceholder", "new search placeholder");
+        return "index";
+    }
+}
---
+++ src/test/java/my/bookshopapp/BookShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package my.bookshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
